9e423f5a2340a4c278b308c4852065bcaae111cf
Earl Mike Sarabia <sarabiaearlmike14@gmail.com>
2023-09-04 23:01:21 +0800

    Initial Change

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { styles } from './style/Styles.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+
+        <View style={[
+          styles.mainheader,
+          {backgroundColor: '#c0392b'}
+        ]}>
+
+        <View style={styles.contentmargin}>
+        <Image 
+        source={require('./src/image/profile.png')} 
+        style={{width: 100, height: 100}}
+        />
+          <Text style={[styles.name,
+            {fontSize: 30, fontWeight: "bold"}
+          ]}>Earl Mike H. Sarabia</Text>
+          <Text style={[styles.name,
+            {fontSize: 20}
+          ]}>Information Technology 3</Text>
+        </View>
+
+      </View>
+        <View style={styles.container2}>
+          <Text style={[
+          {color: 'black' ,fontSize: 25}
+          ]}>My Subjects</Text>
+
+          <View style={styles.subheader}>
+
+          <Text style={styles.subtext}>ELEC 2</Text>
+          <Text style={styles.subtxt2}>Web Development/ Web Enterprise{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>ELEC 3</Text>
+          <Text style={[
+              {marginRight: 123, marginTop: 23}   
+            ]}>Mobile Application{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>IT 311</Text>
+          <Text style={[
+            {marginRight: 105, marginTop: 23}   
+          ]}>Software Engineering{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>IT 312</Text>
+          <Text style={[
+            {marginRight: 27, marginTop: 23, fontSize: 13}   
+          ]}>Information Assurance and Security 2{'\n'}Units: 3</Text>
+          </View>
+
+          <View style={styles.subheader}>
+          <Text style={styles.subtext}>IT 313</Text>
+          <Text style={[
+            {marginRight: 107, marginTop: 23}   
+          ]}>Quantitative Methods{'\n'}Units: 3</Text>
+          </View>
+          <Text style={[
+            {color: '#525FE1' ,
+            fontSize: 15,
+            marginTop: 15,
+            marginLeft: 205}
+            ]}>Click here to see more...</Text>
+      </View>
     </View>
+
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
